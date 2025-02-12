a0894df6f28e70e337133fa58ca836ef1a9d38f9
fpoulin123 <fabrice.poulin7@gmail.com>
2025-02-12 17:12:43 -0500

    Added playing sound at check.

--- src/main/java/memberchecker/Start.java
+++ src/main/java/memberchecker/Start.java
@@ -1,11 +1,18 @@
 package memberchecker;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class Start {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, UnsupportedAudioFileException, IOException, LineUnavailableException {
+		
+		
+		System.out.println("Member checker initialized and ready!");
 		while(true) {
 			Scanner scanner = new Scanner(System.in);
 			String memberNum = scanner.nextLine();
@@ -14,9 +21,12 @@ public class Start {
 			
 			Checker checker = new Checker();
 			String log = checker.checkMemberValidity(memberNum);
+			AudioPlayer audioPlayer = new AudioPlayer();
 			if(log!=null) {
+				audioPlayer.play("./ok.wav");
 				System.out.println(log);
 			}else {
+				audioPlayer.play("./not_ok.wav");
 				System.out.println("Member not found or not valid for number " + memberNum);
 			}
 			
